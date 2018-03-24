adf4ab1a447d279ee40b7c6a1c8b0a41cd227b48
jye64 <jye64@uwo.ca>
2018-03-24 12:14:31 -0400

    enemy_2

---
+++ jye64_SE2250_ProjectPart1/Assets/__Scripts/Enemy_2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Sine Wave Movement
+
+public class Enemy_2 : Enemy {
+
+	[Header("Set in Inspector")]
+	public float waveFrequency =1;
+	public float waveWidth = 2;
+	public float waveRotY = 45;
+
+	private float x0;
+	private float birthTime;
+
+	void Start(){
+		x0 = pos.x;
+		birthTime=Time.time;
+	}
+
+	public override void Move(){
+		Vector3 tempPos = pos;
+		float age = Time.time -birthTime;
+		float theta = Mathf.PI*2*age/waveFrequency;
+		float sin = Mathf.Sin(theta);
+		tempPos.x = x0 + waveWidth * sin;
+		pos = tempPos;
+
+		Vector3 rot = new Vector3(0,sin*waveRotY,0);
+		this.transform.rotation = Quaternion.Euler(rot);
+		base.Move();
+	}
+}
