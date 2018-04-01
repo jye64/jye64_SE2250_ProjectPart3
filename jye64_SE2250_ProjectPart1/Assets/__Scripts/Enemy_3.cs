8278fdd2a9fd037a765f3079ea3c33d81b66ea6a
jye64 <jye64@uwo.ca>
2018-04-01 15:44:39 -0400

    enemy-3 move and fire

--- jye64_SE2250_ProjectPart1/Assets/__Scripts/Enemy_3.cs
+++ jye64_SE2250_ProjectPart1/Assets/__Scripts/Enemy_3.cs
@@ -2,19 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Enemy_3 goes straight down, shoot towards player, then goes up
+
 public class Enemy_3 : Enemy {
 
-	[Header("Set in Inspector: Enemy_3")]
-	public float delayBetweenShot;
+    [Header("Set in Inspector: Enemy_3")]
+    public float projectileSpeed = 40;
+    public float rotateSpeed = 10f;
+    public GameObject projectileEnemy;
+
+    private float timeCounter = 0f;
+
+	void Start(){
+		Invoke ("EnemyFire", 3f);
+	}
+		
+	void EnemyFire()
+    {
+        GameObject gameOB = Instantiate<GameObject>(projectileEnemy);
+        gameOB.transform.position = this.gameObject.transform.position;
+        gameOB.tag = "ProjectileEnemy";
+        gameOB.layer = LayerMask.NameToLayer("ProjectileEnemy");
+
+		GameObject hero = GameObject.Find ("_Hero");
+		Vector3 vel = hero.transform.position -this.transform.position;
+
+        Rigidbody rb = gameOB.GetComponent<Rigidbody>();
+		rb.velocity = vel;
+    }
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public override void Move()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotateSpeed);
+        Vector3 tempPos = pos;
+        if (timeCounter <= 3f)
+        {
+            tempPos.y -= speed * Time.deltaTime;
+            pos = tempPos;
+        }
+        else
+        {
+            tempPos.y += 2*speed * Time.deltaTime;
+            pos = tempPos;
+        }
+        timeCounter += Time.deltaTime;
+
+    }
+
+
 }
+
+
