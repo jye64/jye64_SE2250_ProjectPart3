80e28461a6f2626f8502dae0826ec31decf0cc0c
jye64 <jye64@uwo.ca>
2018-04-01 15:44:26 -0400

    enemy-2 fire

--- jye64_SE2250_ProjectPart1/Assets/__Scripts/Enemy_2.cs
+++ jye64_SE2250_ProjectPart1/Assets/__Scripts/Enemy_2.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//Sine Wave Movement
+//Sine Wave Movement, shoot three bullets after 3 seconds
 
 public class Enemy_2 : Enemy {
 
 	[Header("Set in Inspector: Enemy_2")]
+	public float projectileSpeed = 40;
 	public float waveFrequency =1;
 	public float waveWidth = 2;
 	public float waveRotY = 45;
+	public GameObject projectilePrefab;
 
 	private float x0;
 	private float birthTime;
@@ -17,6 +19,23 @@ public class Enemy_2 : Enemy {
 	void Start(){
 		x0 = pos.x;
 		birthTime=Time.time;
+		Invoke ("EnemyFire", 3f);
+	}
+
+	void EnemyFire(){
+		GameObject[] gameOB = new GameObject[3];
+		for (int i = 0; i <= 2; i++) {
+			gameOB [i] = Instantiate<GameObject> (projectilePrefab);
+			gameOB [i].transform.position = transform.position;
+		}
+		Rigidbody rigidB1 = gameOB [0].GetComponent<Rigidbody> ();
+		Rigidbody rigidB2 = gameOB [1].GetComponent<Rigidbody> ();
+		Rigidbody rigidB3 = gameOB [2].GetComponent<Rigidbody> ();
+
+		rigidB1.velocity = Vector3.down * projectileSpeed;
+		rigidB2.velocity = new Vector3 (-0.5f, -1, 0) * projectileSpeed/2;
+		rigidB3.velocity = new Vector3 (0.5f, -1, 0) * projectileSpeed/2;
+		
 	}
 
 	public override void Move(){
