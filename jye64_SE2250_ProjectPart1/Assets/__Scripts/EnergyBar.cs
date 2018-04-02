e60b8a1becf16eb6156d6c7e1afb462139f02aaf
jye64 <jye64@uwo.ca>
2018-04-02 14:55:32 -0400

    energy bar for power up

---
+++ jye64_SE2250_ProjectPart1/Assets/__Scripts/EnergyBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyBar : MonoBehaviour {
+
+	private Slider energyBar;
+	public float currentHP;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Awake(){
+		energyBar = GetComponent<Slider> ();
+		currentHP = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		energyBar.value = currentHP;
+	}
+
+	public void changeHP(float hp){
+		currentHP += hp;
+	}
+		
+}
