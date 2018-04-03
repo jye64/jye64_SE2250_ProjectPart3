641a041dfabaa1394333326649e4eef6a3400166
jye64 <jye64@uwo.ca>
2018-04-02 22:28:02 -0400

    enemy_4 boss attack

--- jye64_SE2250_ProjectPart1/Assets/__Scripts/Enemy_4.cs
+++ jye64_SE2250_ProjectPart1/Assets/__Scripts/Enemy_4.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_4 : Enemy {
+public class Enemy_4 : Enemy
+{
 
 	[Header("Set in Inspector: Enemy_4")]
 	public float projectileSpeed = 40;
@@ -17,7 +18,7 @@ public class Enemy_4 : Enemy {
 
 	//boss weapon types
 	private string[] weaponDefinition = {
-		"blaster", "spread"
+		"blaster", "track"
 	};
 
 	public override void Awake()
@@ -27,7 +28,7 @@ public class Enemy_4 : Enemy {
 		bndCheck = GetComponent<BoundsCheck>();
 		base.Awake();
 		timer = 0;
-		Invoke ("EnemyFire", 2f);
+		Invoke("EnemyFire", 2f);
 	}
 
 	// Use this for initialization
@@ -43,53 +44,65 @@ public class Enemy_4 : Enemy {
 		}
 	}
 
-	void EnemyFire(){
-		int choice = Random.Range (0, weaponDefinition.Length);
-		switch(choice){
+	void EnemyFire()
+	{
+		int choice = Random.Range(0, weaponDefinition.Length);
+		switch (choice)
+		{
 		case 0:
-			Blaster ();
+			Blaster();
 			break;
 
 		case 1:
-
+			Track();
 			break;
 
 		}
 
-		Invoke ("EnemyFire", 2f);
+		Invoke("EnemyFire", 1.5f);
 	}
 
 	public override void Move()
 	{
-		if (direction == true){
+		if (direction == true)
+		{
 			transform.Translate(new Vector3(-1 * Time.deltaTime * horizontalSpeed, 0, 0)); // left
-			if (pos.x < -camWidth + radius+3.5)
+			if (pos.x < -camWidth + radius + 3.5)
 			{
 				direction = false; //when hits the scene boundary, change direction
 			}
-		} else {
+		}
+		else
+		{
 			transform.Translate(new Vector3(1 * Time.deltaTime * horizontalSpeed, 0, 0)); // right
-			if (pos.x > camWidth - radius-3.5)
+			if (pos.x > camWidth - radius - 3.5)
 			{
 				direction = true; //when hits the scene boundary, change direction
 			}
 		}
-		if(timer<1.5f){
-			base.Move ();
+		if (timer < 1.5f)
+		{
+			base.Move();
 		}
 		timer += Time.deltaTime;
 	}
 
-	void Blaster(){
+	void Blaster()
+	{
 		GameObject[] goes = new GameObject[10];
-		for (int i=0; i<10; i++){
+		for (int i = 0; i < 10; i++)
+		{
 			goes[i] = Instantiate<GameObject>(projectilePrefab);
 		}
-		for (int i=0; i<10; i++){
-			if(i<=4){
-				goes [i].transform.position += this.transform.position + new Vector3 (2, 0, 0) * i;
-			}else{
-				goes [i].transform.position += this.transform.position + new Vector3 (-2, 0, 0) * i;
+		for (int i = 0; i < 10; i++)
+		{
+			if (i <= 4)
+			{
+				goes[i].transform.position += this.transform.position + new Vector3(2, 0, 0) * i;
+			}
+			else
+			{
+				goes[i].transform.position += this.transform.position + new Vector3(-2, 0, 0) * i;
 			}
 			goes[i].tag = "ProjectileEnemy";
 			goes[i].layer = LayerMask.NameToLayer("ProjectileEnemy");
@@ -98,4 +111,33 @@ public class Enemy_4 : Enemy {
 		}
 	}
 
+	void Track()
+	{
+		GameObject[] goes = new GameObject[10];
+		for (int i = 0; i < 10; i++)
+		{
+			goes[i] = Instantiate(projectilePrefab);
+		}
+		for (int i = 0; i < 10; i++)
+		{
+			if (i < 10)
+			{
+				goes[i].transform.position = this.gameObject.transform.position + new Vector3(4, 2, 0) * i;
+			}
+			else
+			{
+				goes[i].transform.position = this.gameObject.transform.position + new Vector3(4, 2, 0) * i;
+			}
+			goes[i].tag = "ProjectileEnemy";
+			goes[i].layer = LayerMask.NameToLayer("ProjectileEnemy");
+			GameObject hero = GameObject.Find("_Hero");
+			if (hero != null)
+			{
+				Vector3 vel = hero.transform.position - this.transform.position;
+				Rigidbody rb = goes[i].GetComponent<Rigidbody>();
+				rb.velocity = vel;
+			}
+		}
+
+	}
 }
